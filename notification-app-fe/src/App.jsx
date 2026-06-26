0d2a0b6aa56ada0ed00030c270ec7c66ae70583d
Srishti Pandey <srishtipandey0512@gmail.com>
2026-06-26 12:58:43 +0530

    react

--- notification-app-fe/src/App.jsx
+++ notification-app-fe/src/App.jsx
@@ -1,3 +1,22 @@
 export default function App() {
   return "Notifications App";
 }
+import { useEffect } from "react";
+import { log } from "./middleware/logger";
+
+export default function App() {
+  useEffect(() => {
+    log(
+      "frontend",
+      "info",
+      "page",
+      "Notifications App Loaded"
+    );
+  }, []);
+
+  return (
+    <div>
+      Notifications App
+    </div>
+  );
+}
